d194f7c43e3f05d38c2d414def63156c12a3a5d7
Karamjeet Sony <141219674+Aayush4532@users.noreply.github.com>
2025-07-12 19:18:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://code-nation-client.vercel.app',
     credentials: true 
 }))
 
